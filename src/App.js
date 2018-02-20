dddf2b7e7b577cb7968e5aa5bee4c53f48d54c2a
Gustavo San Jose <gustavo@coschedule.com>
2018-02-19 20:17:15 -0600

    [FEATURE] Adds spinner and reset function

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import logo from './wordmark.png';
 import './App.css';
 import raffleOptions from './raffleOptions';
+import Spinner from 'react-spinkit';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {players: 1};
+    this.state = {
+      players: 1,
+      isPlaying: false,
+      winner: null,
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.playGame = this.playGame.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   handleChange(event) {
@@ -24,23 +30,47 @@ class App extends Component {
   }
 
   playGame() {
+    this.setState({
+      isPlaying: true,
+    });
     if (this.state.players > raffleOptions.length) {
       alert(`Only ${raffleOptions.length} are available`);
     }
 
-    const optionsToUse = raffleOptions.slice(0, this.state.players);
+    setTimeout(() => {
+      debugger;
+      const optionsToUse = raffleOptions.slice(0, this.state.players);
+      const winner =optionsToUse[Math.floor(Math.random()*optionsToUse.length)];
 
-    const winner =optionsToUse[Math.floor(Math.random()*optionsToUse.length)];
+      this.setState({
+        winner,
+      });
 
+      const dialog = document.getElementById('winner-dialog');
+      dialog.showModal();
+
+      this.setState({ isPlaying: false });
+    }, 5000)
+  }
+
+  reset() {
     this.setState({
-      winner,
+      players: 1,
+      isPlaying: false,
+      winner: null,
     });
-
     const dialog = document.getElementById('winner-dialog');
-    dialog.show();
+    dialog.close();
   }
 
   render() {
+    const isPlaying = this.state.isPlaying;
+    let isPlayingRender = null;
+    if (isPlaying) {
+      isPlayingRender = <Spinner name="double-bounce" className="App"/>;
+    } else {
+      isPlayingRender = <button disabled={!this.state.players} onClick={this.playGame}>LET'S GO!</button>;
+    }
 
     return (
       <div className="App">
@@ -56,12 +86,12 @@ class App extends Component {
           </label>
         </p>
 
-        <button disabled={!this.state.players} onClick={this.playGame}>LET'S GO!</button>
+        {isPlayingRender}
         
         <dialog id="winner-dialog">  
           <h2>{this.state.winner}</h2>
           <p>YOU ARE THE WINNER!</p>
-          <button onClick>Play again</button>
+          <button onClick={this.reset}>Play Again</button>
         </dialog> 
       </div>
     );
